512977b5f3b4a391a696e74eaa81f56089cf990f
Serhii V <sjcris.me@gmail.com>
2019-12-01 22:37:14 +0200

    add layout for blog card, save blog form

--- src/app/dashboard/blog-add-page/blog-add-page.component.ts
+++ src/app/dashboard/blog-add-page/blog-add-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { Blog } from '../../shared/models/blog.model';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-add-page',
@@ -15,14 +18,25 @@ export class BlogAddPageComponent implements OnInit {
   blogForm: FormGroup;
   isSubmitting = false;
   files = [];
+  user: any;
+  id: number;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private afs: AngularFirestore,
+    private fb: FormBuilder,
+    private router: Router) {
+
+    afs.firestore.settings({ timestampsInSnapshots: true });
     this.blogForm = this.fb.group({
       title: ['', [Validators.required]],
     });
   }
 
   ngOnInit() {
+    // TODO: getUser
+    this.user = { uid: 'mNJTSg1wQGcSvCBLIB7E5LXhZ8H3', email: 'sergeyver@nitka.com', iuser: 'LGykuFrzezHZo1h74' };
+    // TODO: getID from list of all blogs lates
+    this.id = 7;
   }
 
   clear() {
@@ -39,8 +53,23 @@ export class BlogAddPageComponent implements OnInit {
 
   submitForm() {
     this.isSubmitting = true;
-    console.log('f', this.files);
-    console.log('c', this.content);
-    console.log('H', this.blogForm);
+    if (this.blogForm.invalid) {
+      console.log('invalid Form');
+      return;
+    }
+    // console.log('Title', this.blogForm.value.title);
+    // console.log('f', this.files);
+    // console.log('c', this.content);
+    // console.log('H', this.blogForm);
+
+    this.afs.collection('blog').add({
+      'title': this.blogForm.value.title,
+      'body': this.content,
+      'id': this.id,
+      'url': this.files,
+      'userID': this.user.email
+    });
+    // this.blogCollection.add();
+    this.router.navigate(['/dashboard/blog']);
   }
 }
