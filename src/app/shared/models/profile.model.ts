c713111eca78a7bd8173d3e9e50d8ff6d7aadc19
Andrii Ishchenko <aishchenko@deloitte.com>
2020-06-17 23:09:34 +0300

    add tile style and edit character menu

--- src/app/shared/models/profile.model.ts
+++ src/app/shared/models/profile.model.ts
@@ -4,3 +4,45 @@ export class Profile {
     charecterAvatarURL: string;  // string  если пустой используется googleAvatarURL
     mainCharecters: string;
 }
+
+
+export interface FullCharacterModel {
+    authUserEmail: string;
+    name: string;
+    equipmentLevel: string;
+    sex: SexOfCharactersModel;
+    fraction: FractionOfCharacterModel;
+    class: ClassOfCharacterModel;
+    race: RaceOfCharacterModel;
+    sexId: number;
+    fractionId: number;
+    classId: number;
+    raceId: number;
+    docId?: string;
+}
+
+export interface ClassOfCharacterModel {
+    id: number;
+    name: string;
+    icon: string;
+    parent: number[];
+}
+
+export interface RaceOfCharacterModel {
+    id: number;
+    name: string;
+    icon: string;
+    fraction: number[];
+}
+export interface FractionOfCharacterModel {
+    id: number;
+    name: string;
+    icon: string;
+    flag: string;
+}
+
+export interface SexOfCharactersModel {
+    id: number;
+    name: string;
+    icon: string;
+}
