90d6f3f7a5945aab322a7b51f29b2b62a324c2a9
Serhii Verbitskiy <sjcris.me@gmail.com>
2020-06-18 20:51:08 +0300

    fix and rewrite all to Rxjs

--- src/app/dashboard/components/character-modal/character-modal.component.ts
+++ src/app/dashboard/components/character-modal/character-modal.component.ts
@@ -8,12 +8,9 @@ import {
   sexOfCharactersConstnt
 } from '../../../shared/models/constants';
 import { Character } from '../../../shared/models/blog.model';
-import { map } from 'rxjs/internal/operators/map';
-import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
-import { merge } from 'rxjs-compat/operator/merge';
-import { tap, withLatestFrom } from 'rxjs/operators';
-import { startWith } from 'rxjs-compat/operator/startWith';
-import { combineLatest } from 'rxjs-compat/operator/combineLatest';
+
+import { Observable, of, Subscription, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-character-modal',
@@ -23,13 +20,11 @@ import { combineLatest } from 'rxjs-compat/operator/combineLatest';
 })
 export class CharacterModalComponent implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription();
-  //raceOfCharacters$: Observable<any>;
-  //constStream$ = of(raceOfCharactersConstnt);
+  raceOfCharacters$: Observable<any>;
 
   characterForm: FormGroup;
   characterData: Character = null;
   classOfCharacters = classOfCharactersConstnt;
-  raceOfCharacters = raceOfCharactersConstnt;
   fractionOfCharacters = fractionOfCharactersConstnt;
   sexOfCharacters = sexOfCharactersConstnt;
 
@@ -42,7 +37,6 @@ export class CharacterModalComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.initForm();
-    this.updateFormParams();
   }
 
   get isEditMod () {
@@ -60,43 +54,34 @@ export class CharacterModalComponent implements OnInit, OnDestroy {
       sexId: [this.characterData ? this.characterData.sexId : '' , [Validators.required]],
       fractionId: [this.characterData ? this.characterData.fractionId : '' , [Validators.required]],
       classId: [this.characterData ? this.characterData.classId : '' , [Validators.required]],
-      raceId: [this.characterData ? this.characterData.raceId : '' , [Validators.required]]
+      raceId: [{ value: null, disabled: true } , [Validators.required]],
     });
+
+    if (this.characterData) {
+      this.characterForm.get('raceId').enable({onlySelf: true});
+      const value = [...raceOfCharactersConstnt].find(c => c.id == this.characterData.raceId);
+      this.raceOfCharacters$ = of( [value]);
+      this.characterForm.get('raceId').setValue(value, {onlySelf: true});
+
+    } else {
+      this.updateFormParams();
+    }
   }
 
-  // TODO: not work on update existing character
   updateFormParams () {
-        // const fraction$ = this.characterForm.get('fractionId').valueChanges;
-        // const classId$  = this.characterForm.get('classId').valueChanges;
 
-    // this.subscription.add(
-    //   merge(fraction$, classId$, this.constStream$).pipe().subscribe(x => console.log(x))
-    //   combineLatest(fraction$, classId$).pipe().subscribe( x => console.log(x))
-     //);
-    // this.raceOfCharacters$ = combineLatest(fraction$, classId$);
+    const fraction$ = this.characterForm.get('fractionId').valueChanges;
+    const classId$  = this.characterForm.get('classId').valueChanges;
 
-
-    this.subscription.add(
-      this.characterForm.get('fractionId').valueChanges.pipe(
-        map(val => {
-
-          this.raceOfCharacters = [...raceOfCharactersConstnt].filter(char => char.fraction.includes(val));
-          console.log(this.raceOfCharacters);
-          return val;
+    this.raceOfCharacters$ = combineLatest([fraction$, classId$])
+      .pipe(
+        map(i => {
+          this.characterForm.get('raceId').enable({onlySelf: true});
+          const sortbyRace = [...raceOfCharactersConstnt].filter(char => char.fraction.includes(i[0]));
+          const sortbyClass = [...classOfCharactersConstnt].filter(classid => classid.id === i[1])[0].parent;
+          return sortbyRace.filter(char => sortbyClass.some(a => char.id === a));
         })
-      ).subscribe(x => console.log('sub', x))
-    );
-    //
-    // /// TODO: fix if user select class and then change
-    this.subscription.add(
-      this.characterForm.get('classId').valueChanges.pipe().subscribe(x => {
-        const classOfChar = Array.from([...classOfCharactersConstnt].filter(classid => classid.id === x)[0].parent);
-        let items = [];
-        classOfChar.forEach(i => items.push(i));
-
-        this.raceOfCharacters = [...this.raceOfCharacters].filter(char => items.some(i => i === char.id));
-      })
-    );
+      );
   }
 
   getUrl(iconType: string, iconName: string) {
@@ -106,6 +91,9 @@ export class CharacterModalComponent implements OnInit, OnDestroy {
   addNewCharacter() {
     if (this.characterForm.valid) {
       const formValue = this.characterForm.value;
+      const {raceId} = this.characterForm.value;
+      this.characterForm.value.raceId = raceId.id;
+
       this.dialogRef.close(formValue);
     } else {
       this.characterForm.markAllAsTouched();
@@ -115,6 +103,9 @@ export class CharacterModalComponent implements OnInit, OnDestroy {
   updateCharacter(): void {
     if (this.characterForm.valid) {
       const formValue = this.characterForm.value;
+      const {raceId} = this.characterForm.value;
+      this.characterForm.value.raceId = raceId.id;
+
       this.dialogRef.close({...formValue, docId: this.characterData.docId});
     } else {
       this.characterForm.markAllAsTouched();
